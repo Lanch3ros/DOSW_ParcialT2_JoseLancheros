9d398449430062e6ddbe79baab5a523bc83f23e2
unknown <jose.lancheros-a@mail.escuelaing.edu.co>
2026-04-10 11:45:36 -0500

    feat: complete Orderrepository para saber los estados de los pedidos

--- src/main/java/edu/dosw/parcial/persistence/repositories/OrderRepository.java
+++ src/main/java/edu/dosw/parcial/persistence/repositories/OrderRepository.java
@@ -1,4 +1,14 @@
 package edu.dosw.parcial.persistence.repositories;
 
-public class OrderRepository {
+import edu.dosw.parcial.core.models.OrderStatus;
+import edu.dosw.parcial.persistence.entities.OrderEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
+
+    Optional<OrderEntity> findByUserIdAndStatusIn(Long userId, List<OrderStatus> statuses);
+
+    List<OrderEntity> findByUserId(Long userId);
 }
