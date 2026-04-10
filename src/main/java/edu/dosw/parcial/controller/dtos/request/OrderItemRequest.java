3e54e64b36033774b5248ae7ede7452a244ee944
unknown <jose.lancheros-a@mail.escuelaing.edu.co>
2026-04-10 11:59:51 -0500

    feat: complete OrderItemRequest

--- src/main/java/edu/dosw/parcial/controller/dtos/request/OrderItemRequest.java
+++ src/main/java/edu/dosw/parcial/controller/dtos/request/OrderItemRequest.java
@@ -1,4 +1,15 @@
 package edu.dosw.parcial.controller.dtos.request;
 
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+@Data
 public class OrderItemRequest {
+
+    @NotBlank(message = "El código QR es obligatorio")
+    private String qrCode;
+
+    @NotNull(message = "La cantidad es obligatoria")
+    @Min(value = 1, message = "La cantidad debe ser mayor a 0")
+    private Integer quantity;
 }
