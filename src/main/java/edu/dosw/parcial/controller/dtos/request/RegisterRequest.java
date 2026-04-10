bc19f4446a2db95b104ad6e714a670ab8fc168bc
unknown <jose.lancheros-a@mail.escuelaing.edu.co>
2026-04-10 11:53:51 -0500

    feat: complete RegisterRequest con cada campo siendo obligatorio y con un mensaje

--- src/main/java/edu/dosw/parcial/controller/dtos/request/RegisterRequest.java
+++ src/main/java/edu/dosw/parcial/controller/dtos/request/RegisterRequest.java
@@ -1,4 +1,20 @@
 package edu.dosw.parcial.controller.dtos.request;
 
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+@Data
 public class RegisterRequest {
+
+    @NotBlank(message = "El nombre completo es obligatorio")
+    @Size(max = 100, message = "El nombre no puede superar 100 caracteres")
+    private String fullName;
+
+    @NotBlank(message = "El email es obligatorio")
+    @Email(message = "Formato de email inválido")
+    private String email;
+
+    @NotBlank(message = "La contraseña es obligatoria")
+    @Size(min = 8, message = "La contraseña debe tener al menos 8 caracteres y debe contener al menos una mayúscula y un número")
+    private String password;
 }
