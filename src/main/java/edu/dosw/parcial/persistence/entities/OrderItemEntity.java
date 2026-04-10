3d9861b2ecdfebbd40103ff78087669ac1ca5fe5
unknown <jose.lancheros-a@mail.escuelaing.edu.co>
2026-04-10 11:39:08 -0500

    feat: añadi los campos para OrderItemEntity

--- src/main/java/edu/dosw/parcial/persistence/entities/OrderItemEntity.java
+++ src/main/java/edu/dosw/parcial/persistence/entities/OrderItemEntity.java
@@ -1,4 +1,32 @@
 package edu.dosw.parcial.persistence.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "order_items")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 public class OrderItemEntity {
+
+    @Id
+    private long id;
+
+    @JoinColumn(name = "order_id", nullable = false)
+    private OrderEntity order;
+
+    @Column(name = "product_id", nullable = false)
+    private long productId;
+
+    @Column(name = "product_name", nullable = false)
+    private String productName;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(name = "unit_price", nullable = false)
+    private BigDecimal unitPrice;
+
+    @Column(nullable = false)
+    private BigDecimal subtotal;
 }
