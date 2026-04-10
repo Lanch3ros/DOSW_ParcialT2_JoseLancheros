6504fd712568470a6ddea83cccaae7a17c808c33
unknown <jose.lancheros-a@mail.escuelaing.edu.co>
2026-04-10 11:15:53 -0500

    feat: complete userEntity con sus campos que pide y las anotaciones que describen cada columna

--- src/main/java/edu/dosw/parcial/persistence/entities/UserEntity.java
+++ src/main/java/edu/dosw/parcial/persistence/entities/UserEntity.java
@@ -1,4 +1,33 @@
 package edu.dosw.parcial.persistence.entities;
 
+import edu.dosw.parcial.core.models.UserRole;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
 }
