2091d6d8e11ff58ccb9ebdae7c9e39fcb3759495
unknown <jose.lancheros-a@mail.escuelaing.edu.co>
2026-04-10 12:13:06 -0500

    feat: complete UserResponse

--- src/main/java/edu/dosw/parcial/controller/dtos/response/UserResponse.java
+++ src/main/java/edu/dosw/parcial/controller/dtos/response/UserResponse.java
@@ -1,4 +1,13 @@
 package edu.dosw.parcial.controller.dtos.response;
 
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Data @Builder
 public class UserResponse {
+    private String id;
+    private String fullName;
+    private String email;
+    private String role;
+    private LocalDateTime createdAt;
 }
