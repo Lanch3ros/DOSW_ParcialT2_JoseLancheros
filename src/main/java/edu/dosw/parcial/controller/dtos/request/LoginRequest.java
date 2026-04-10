026637f67c1f030c697ccaa0896b1b08325f6524
unknown <jose.lancheros-a@mail.escuelaing.edu.co>
2026-04-10 11:57:44 -0500

    feat: complete LoginRequest con email y la clave, ambos deben ser obligatorios

--- src/main/java/edu/dosw/parcial/controller/dtos/request/LoginRequest.java
+++ src/main/java/edu/dosw/parcial/controller/dtos/request/LoginRequest.java
@@ -1,4 +1,15 @@
 package edu.dosw.parcial.controller.dtos.request;
 
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+@Data
 public class LoginRequest {
+
+    @NotBlank(message = "El email es obligatorio")
+    @Email(message = "Formato de email inválido")
+    private String email;
+
+    @NotBlank(message = "La contraseña es obligatoria")
+    private String password;
 }
