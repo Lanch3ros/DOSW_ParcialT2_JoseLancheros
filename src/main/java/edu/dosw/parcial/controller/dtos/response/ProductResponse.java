2034436f37d9c38273779408fc81c4115597bbfb
unknown <jose.lancheros-a@mail.escuelaing.edu.co>
2026-04-10 12:28:23 -0500

    feat: complete ProductResponse

--- src/main/java/edu/dosw/parcial/controller/dtos/response/ProductResponse.java
+++ src/main/java/edu/dosw/parcial/controller/dtos/response/ProductResponse.java
@@ -1,4 +1,15 @@
 package edu.dosw.parcial.controller.dtos.response;
 
+import lombok.*;
+import java.math.BigDecimal;
+
+@Data @Builder
 public class ProductResponse {
+    private String id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private String qrCode;
+    private Integer stock;
+    private String status;
 }
