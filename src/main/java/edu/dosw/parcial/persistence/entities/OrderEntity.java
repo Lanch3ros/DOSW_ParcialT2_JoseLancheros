fabdd4633eddc208b083e8ae0e47a1d39aa3d49e
unknown <jose.lancheros-a@mail.escuelaing.edu.co>
2026-04-10 11:34:26 -0500

    feat: añadi los campos para OrderEntity, recordar que tenemos una lista items para guardar todos los productos

--- src/main/java/edu/dosw/parcial/persistence/entities/OrderEntity.java
+++ src/main/java/edu/dosw/parcial/persistence/entities/OrderEntity.java
@@ -1,4 +1,37 @@
 package edu.dosw.parcial.persistence.entities;
 
+import edu.dosw.parcial.core.models.OrderStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 public class OrderEntity {
+
+    @Id
+    private long id;
+
+    @Column(name = "user_id", nullable = false)
+    private long userId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus status;
+
+    @Column(nullable = false)
+    private BigDecimal total;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItemEntity> items = new ArrayList<>();
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
